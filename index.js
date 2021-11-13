2e2d13802b242f3fc6af5bc6fca72ad2f2f200c2
imhossen <imhossen@pm.me>
2021-11-13 19:08:06 +0600

    minor fix

--- index.js
+++ index.js
@@ -113,6 +113,8 @@ async function run() {
       res.send(result);
     });
 
+    //DELETE API
+
     app.delete("/deleteProduct/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
